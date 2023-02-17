3291581e927f90ba95e397f8a16530e66428573b
oOBobbyOo <944831575.com>
2023-02-17 15:43:15 +0800

    test: add vitest

--- packages/hbui-vue/vite.config.ts
+++ packages/hbui-vue/vite.config.ts
@@ -1,11 +1,18 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // jsx 依赖
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   plugins: [vueJsx()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    transformMode: {
+      web: [/\.[jt]sx$/]
+    }
+  },
   resolve: {
     alias: [
       { find: '@hbui', replacement: resolve(__dirname, './ui') },
