fcdb5120c074e66356cb47dee119dd8984ee3ee6
oOBobbyOo <944831575.com>
2023-03-01 14:39:27 +0800

    style(docs): change vitepress custom theme style

--- packages/hbui-vue/docs/.vitepress/theme/index.ts
+++ packages/hbui-vue/docs/.vitepress/theme/index.ts
@@ -1,23 +1,15 @@
-import DefaultTheme from 'vitepress/theme'
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
-import type { Theme } from 'vitepress'
-
 import HbUITheme from '../hbui-theme'
 
 // @ts-ignore
-import Demo from '../demo/Demo.vue'
-// @ts-ignore
-import DemoBlock from '../demo/DemoBlock.vue'
-// @ts-ignore
 import HbUI from '@hbui/vue-hbui'
 
-export default <Theme>{
-  ...DefaultTheme,
+import { registerComponents } from './regComponents.js'
+
+export default {
   ...HbUITheme,
   enhanceApp({ app }) {
     app.use(HbUI)
-
-    app.component('Demo', Demo)
-    app.component('DemoBlock', DemoBlock)
+    registerComponents(app)
   }
 }
